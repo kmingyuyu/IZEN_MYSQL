10b4627d13d82553efe604526baeea541a5b9252
kmingyuyu <skyjangking@naver.com>
2023-07-12 17:52:03 +0900

    up

---
+++ SHOP.sql
@@ -0,0 +1,28 @@
+use shop;
+
+
+select * from member;
+
+update member set role='ADMIN' where member_id = 1;
+
+commit;
+
+select data.num num, item.item_id id, item.item_nm itemNm, item.price price, item_img.img_url imgUrl, item_img.repimg_yn repimgYn 
+            from item 
+			inner join item_img on (item.item_id = item_img.item_id)
+			inner join (select @ROWNUM:=@ROWNUM+1 num, groupdata.* from
+			            (select order_item.item_id, count(*) cnt
+			              from order_item
+			              inner join orders on (order_item.order_id = orders.order_id)
+			              where orders.order_status = 'ORDER'
+			             group by order_item.item_id order by cnt desc) groupdata,
+                          (SELECT @ROWNUM:=0) R 
+                          limit 6) data
+			on (item.item_id = data.item_id)
+			where item_img.repimg_yn = 'Y'
+			order by num;
+
+
+
+
+
